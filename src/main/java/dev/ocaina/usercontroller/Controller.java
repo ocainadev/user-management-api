415d26dd6dfc0aaec5c8c0ef28d740a5e35c8368
Cainã Santos <ocainasantos@gmail.com>
2026-03-06 15:08:54 -0300

    Controller created

---
+++ src/main/java/dev/ocaina/usercontroller/Controller.java
@@ -0,0 +1,16 @@
+package dev.ocaina.usercontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String Welcome() {
+        return "Welcome to Spring Boot!";
+    }
+
+}
