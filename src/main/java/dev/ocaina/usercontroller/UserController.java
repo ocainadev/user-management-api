9ba1e8291b2437bce2a71a2d301738f43c992705
Cainã Santos <ocainasantos@gmail.com>
2026-03-09 14:11:16 -0300

    created userModel, and created entity in database.

--- src/main/java/dev/ocaina/usercontroller/UserController.java
+++ src/main/java/dev/ocaina/usercontroller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class UserController {
 
     @GetMapping("/welcome")
     public String Welcome() {
---
+++ src/main/java/dev/ocaina/usercontroller/UserModel.java
@@ -0,0 +1,44 @@
+package dev.ocaina.usercontroller;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_users")
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private int age;
+
+    public UserModel() {}
+    public UserModel(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
