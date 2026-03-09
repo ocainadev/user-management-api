0185146dab523970f67fed8cb04c197b50ad5e0b
Cainã Santos <ocainasantos@gmail.com>
2026-03-09 17:20:38 -0300

    feat: coloca lombock nas classes

--- src/main/java/dev/ocaina/usercontroller/Tasks/TaskModel.java
+++ src/main/java/dev/ocaina/usercontroller/Tasks/TaskModel.java
@@ -2,11 +2,17 @@ package dev.ocaina.usercontroller.Tasks;
 
 import dev.ocaina.usercontroller.Users.UserModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name="tb_tasks")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TaskModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/dev/ocaina/usercontroller/Users/UserModel.java
+++ src/main/java/dev/ocaina/usercontroller/Users/UserModel.java
@@ -1,11 +1,15 @@
 package dev.ocaina.usercontroller.Users;
 import dev.ocaina.usercontroller.Tasks.TaskModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,34 +26,4 @@ public class UserModel {
     @JoinColumn(name= "tasks_id")
     private TaskModel tasks;
 
-    public UserModel() {}
-    public UserModel(String name, String email, int age) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
