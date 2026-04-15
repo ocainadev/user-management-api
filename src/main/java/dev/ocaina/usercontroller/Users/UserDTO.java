70691cfdf0a0b309eb5b1efb373cdbf520e0b096
Cainã Santos <ocainasantos@gmail.com>
2026-04-14 22:53:41 -0300

    chore: add mapper and dto for user.

--- src/main/java/dev/ocaina/usercontroller/Users/UserDTO.java
+++ src/main/java/dev/ocaina/usercontroller/Users/UserDTO.java
@@ -1,5 +1,19 @@
 package dev.ocaina.usercontroller.Users;
 
+import dev.ocaina.usercontroller.Tasks.TaskModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UserDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private int age;
+    private TaskModel tasks;
+    private String rank;
 
 }
